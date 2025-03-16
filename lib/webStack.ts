3791be517996f32657a0ecda58a2e22b55bcda20
Matt Shanahan <matthew.shanahan@huskers.unl.edu>
2025-03-16 16:44:17 -0700

    fix cloudfront oac

--- lib/webStack.ts
+++ lib/webStack.ts
@@ -1,10 +1,11 @@
-import { App, RemovalPolicy, Stack, StackProps, CfnOutput, aws_cloudfront_origins, aws_iam as iam } from 'aws-cdk-lib'
+import { App, RemovalPolicy, Stack, StackProps, CfnOutput, aws_iam as iam } from 'aws-cdk-lib';
 import * as path from 'path';
 import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import { BlockPublicAccess, Bucket, CfnBucketPolicy } from 'aws-cdk-lib/aws-s3';
+import { Distribution, S3OriginAccessControl, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { BucketDeployment } from 'aws-cdk-lib/aws-s3-deployment';
 
 interface WebAppStackProps extends StackProps {
   stage: string;  // 'beta' or 'prod'
@@ -14,43 +15,34 @@ export class Webstack extends Stack {
   constructor(scope: Construct, id: string, props: WebAppStackProps) {
     super(scope, id, props);
 
-    const siteBucket = new s3.Bucket(this, `WebAppBucket-${props.stage}`, {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    // ✅ 1️⃣ Create the S3 Bucket First
+    const siteBucket = new Bucket(this, `WebAppBucket-${props.stage}`, {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
       removalPolicy: props.stage === 'prod' ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
       autoDeleteObjects: props.stage !== 'prod',
-      websiteIndexDocument: 'index.html',
       enforceSSL: true,
     });
 
-    // Create CloudFront distribution
-    const distribution = new cloudfront.Distribution(this, `WebAppDistribution-${props.stage}`, {
+    // 2. Create a CloudFront distribution with OAC
+    const distribution = new Distribution(this, 'MyDistribution', {
       defaultBehavior: {
-        origin: origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
+        origin: S3BucketOrigin.withOriginAccessControl(siteBucket, {
+          originAccessControl: new S3OriginAccessControl(this, 'MyOAC'),
+        }),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
+      defaultRootObject: 'index.html', // Or your desired default file
     });
 
-    // ✅ Add S3 Bucket Policy to Allow CloudFront OAC Access
-    siteBucket.addToResourcePolicy(new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
-      actions: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject'],
-      resources: [`${siteBucket.bucketArn}/*`],
-      conditions: {
-        StringEquals: {
-          'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`
-        }
-      }
-    }));
-
-    // Deploy web app to S3
-    new s3deploy.BucketDeployment(this, `DeployWebApp-${props.stage}`, {
-      sources: [s3deploy.Source.asset(path.resolve(__dirname, '../../mariia-site/dist'))], // Make sure React is built before deployment
+    // ✅ 6️⃣ Deploy Web App to S3
+    new BucketDeployment(this, `DeployWebApp-${props.stage}`, {
+      sources: [Source.asset(path.resolve(__dirname, '../../mariia-site/dist'))], // Ensure React is built before deployment
       destinationBucket: siteBucket,
       distribution,
       distributionPaths: ['/*'],
     });
 
-    // Output the distribution URL
+    // ✅ 7️⃣ Output CloudFront Distribution URL
     new CfnOutput(this, 'DistributionURL', {
       value: distribution.distributionDomainName,
     });
